e2c1d741a2187a6c200ba05ac27e0d9ab13ec184
Mvteiio <romancamargo02@gmail.com>
2025-09-25 13:30:02 -0500

    Feat :bug: Fixing Bugs

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.json());
 const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://localhost:5500',
-    'https://andres8073562.github.io/' 
+    'https://andres8073562.github.io' 
 ];
 
 const corsOptions = {
